51e66926c7d36e9026e32de60a9c4bb286806e77
JF195 <112326931+JF195@users.noreply.github.com>
2026-01-11 11:53:02 -0500

    allow animation of checkerboard

--- widgets/checkerboard.py
+++ widgets/checkerboard.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from widget import Widget as WidgetBase
 from widget_config_item import WidgetConfigItem as Config
 from widget_config_item import ConfigItemType as ConfigType
@@ -9,6 +10,8 @@ class Widget(WidgetBase):
     desired_spf = -1
     allow_rotation = False
 
+    start_time = datetime.now().timestamp()
+
     def __init__(self):
         super().__init__()
         self.configuration = {
@@ -18,22 +21,30 @@ class Widget(WidgetBase):
             "Brightness 2": Config(ConfigType.integer, 0, 0, 255),
             "Board Size": Config(ConfigType.integer, 1, 1, WIDTH),
             "X Offset": Config(ConfigType.integer, 0, 0, WIDTH),
-            "Y Offset": Config(ConfigType.integer, 0, 0, HEIGHT)
+            "Y Offset": Config(ConfigType.integer, 0, 0, HEIGHT),
+            "Time Between Updates (sec)": Config(ConfigType.integer, -1, -1, 30),
+            "X Speed (px per s)": Config(ConfigType.integer, 0, -10, 10),
+            "Y Speed (px per s)": Config(ConfigType.integer, 0, -10, 10)
         }
 
     def get_current_size(self):
         return [self.configuration["Board Width"].value, self.configuration["Board Height"].value]
     
     def get_desired_spf(self):
-        return -1
+        return self.configuration["Time Between Updates (sec)"].value
 
     def get_frame(self):
         width = self.configuration["Board Width"].value
         height = self.configuration["Board Height"].value
         size = self.configuration["Board Size"].value
 
-        xoff = self.configuration["X Offset"].value
-        yoff = self.configuration["Y Offset"].value
+        xspeed = self.configuration["X Speed (px per s)"].value
+        yspeed = self.configuration["Y Speed (px per s)"].value
+
+        delta_time = float(datetime.now().timestamp() - self.start_time)
+
+        xoff = self.configuration["X Offset"].value - int(xspeed * delta_time) % (size << 1)
+        yoff = self.configuration["Y Offset"].value - int(yspeed * delta_time) % (size << 1)
 
         base = np.matrix([[ self.configuration["Brightness 1"].value if ( (((x + xoff) // size) % 2) + (((y + yoff) // size) % 2) != 1 ) else self.configuration["Brightness 2"].value for x in range(width)] for y in range(height)]) # type: ignore
 
